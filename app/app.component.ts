6e68496518a1dedf7dfb705176f0715e53bd1c68
krkchaitanya <krkchaitanya42@gmail.com>
2017-08-02 22:37:00 -0500

    first

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+@Component({
+  selector:"app-root",
+  templateUrl:"./app.component.html",
+  styleUrls:["./app.component.css"]
+})
+
+export class  AppComponent {
+answere="";
+
+onSubmit(form: NgForm){
+  console.log(form);
+}
+}
